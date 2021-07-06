dd9fc1c270a4f1e58bdecbf54bd1b10eff6a18e7
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-07-06 17:21:08 -0300

    Update working.py

--- Sudoku/working.py
+++ Sudoku/working.py
@@ -25,4 +25,8 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-print_board(board)
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
