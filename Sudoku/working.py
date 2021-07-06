21193020e445b63fa7b8996c5e9bf89c4d0c8b1b
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-07-06 17:17:49 -0300

    Update working.py

--- Sudoku/working.py
+++ Sudoku/working.py
@@ -13,8 +13,8 @@ board = [
 def print_board(bo):
 
     for i in range(len(bo)):
-        if i % 3 and i != 0:
-            print("- " * 13)
+        if i % 3 == 0 and i != 0:
+            print("- " * 12)
 
         for j in range(len(bo[0])):
             if j % 3 == 0 and j != 0:
