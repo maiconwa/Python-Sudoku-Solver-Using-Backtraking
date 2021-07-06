13197ccdcdf46dec572edb42fc4b5a737a83a481
Maicon William <73193175+maiconwa@users.noreply.github.com>
2021-07-06 17:36:40 -0300

    Box check

--- Sudoku/working.py
+++ Sudoku/working.py
@@ -23,10 +23,13 @@ def valid(bo, num, pos):
             return False
 
     # Check box
-    box x =
-    box y =
-    
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
 
 def print_board(bo):
 
